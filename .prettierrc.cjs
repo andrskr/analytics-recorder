5c9d94e9e7a8db0726d1278d3a20edf5de71ced4
Andrii Skrebets <andriiskrebets@gmail.com>
2022-12-14 10:28:53 +0200

    setup prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  insertPragma: false,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  endOfLine: 'auto',
+};
